32910daa2275dbe2fdc33154ee4ca469d09af3c4
oananovac <novac.oana@yahoo.com>
2025-09-28 14:05:51 +0300

    First commit

---
+++ src/main/java/com/example/ss_2022_c21_e1/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.ss_2022_c21_e1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails u1 = User.withUsername("bill")
+                .password(passwordEncoder().encode("12345"))
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(u1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // you can use this in a real world scenario
+    }
+}
