d097ebcdc0c353770075ca7a8493686e79cff913
oananovac <novac.oana@yahoo.com>
2025-09-28 14:06:19 +0300

    Add authentication test

--- src/test/java/com/example/ss_2022_c21_e1/Ss2022C21E1ApplicationTests.java
+++ src/test/java/com/example/ss_2022_c21_e1/Ss2022C21E1ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.ss_2022_c21_e1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Ss2022C21E1ApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    void test1() throws Exception {
+        mvc.perform(get("/demo")
+                        .with(httpBasic("bill","12345")))
+                .andExpect(status().isOk());
     }
 
 }
