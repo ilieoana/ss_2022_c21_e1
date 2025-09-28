32e67691e9b1880baf779930fe6af024639221ac
oananovac <novac.oana@yahoo.com>
2025-09-28 14:12:04 +0300

    Add csrf protection test

--- src/main/java/com/example/ss_2022_c21_e1/controllers/DemoController.java
+++ src/main/java/com/example/ss_2022_c21_e1/controllers/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.ss_2022_c21_e1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class DemoController {
     @GetMapping("/demo")
     public String demo(){
         return "Demo";
+    }
+
+    @PostMapping("/hello")
+    public String hello(){
+        return "Hell";
     }
 }
--- src/test/java/com/example/ss_2022_c21_e1/Ss2022C21E1ApplicationTests.java
+++ src/test/java/com/example/ss_2022_c21_e1/Ss2022C21E1ApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -21,6 +22,14 @@ class Ss2022C21E1ApplicationTests {
     void test1() throws Exception {
         mvc.perform(get("/demo")
                         .with(httpBasic("bill","12345")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void test2() throws Exception {
+        mvc.perform(post("/hello")
+                        .with(httpBasic("bill","12345"))
+                        .with(csrf()))
                 .andExpect(status().isOk());
     }
 
